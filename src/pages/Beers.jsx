f173e9f265700d6cc78a3bad80207ed40fb77030
AaronBarcos <abarcoscaballero@gmail.com>
2023-03-05 12:28:51 +0100

    busqueda

--- src/pages/Beers.jsx
+++ src/pages/Beers.jsx
@@ -1,66 +1,86 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Beers() {
+  const [allBeers, setAllBeers] = useState(null);
+  const [beerFiltered, setBeerFiltered] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
 
- const [allBeers, setAllBeers] = useState(null)
- const [searchInput, setSearchInput] = useState("")
+  useEffect(() => {
+    getDataBeers();
+  }, []);
 
- useEffect(() => {
-  getDataBeers()
- }, [])
- 
- const getDataBeers = async() => {
-  try {
-    const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers")
-    setAllBeers(response.data)
-    console.log(response.data)
-  } catch (error) {
-    console.log(error)
+  const getDataBeers = async () => {
+    try {
+      const response = await axios.get(
+        "https://ih-beers-api2.herokuapp.com/beers"
+      );
+      setAllBeers(response.data);
+      setBeerFiltered(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearchInput = (event) => {
+    setSearchInput(event.target.value);
+    filterBeers(event.target.value)
+  };
+
+  const filterBeers = (search) => {
+    let filterResults = beerFiltered.filter((eachBeer) => {
+      if (eachBeer.name.includes(search)) {
+        return eachBeer
+      }
+    })
+    setAllBeers(filterResults)
   }
- }
 
- const handleSearchInput = async (event) => {
-  try {
-    setSearchInput(event.target.value)
-    const response = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${searchInput}`)
-    setAllBeers(response.data)
-  } catch (error) {
-    console.log(error)
+  if (allBeers === null) {
+    return (
+      <>
+        <h1>Wait...</h1>
+      </>
+    );
   }
- }
-
-
-if (allBeers === null) {
   return (
-    <>
-      <h1>Wait...</h1>
-    </>
-  )
-
-}
-  return (
-    <div style={{marginLeft: 40, marginTop: 30, display: 'flex', flexDirection: 'column', gap: 15 }}>
+    <div
+      style={{
+        marginLeft: 40,
+        marginTop: 30,
+        display: "flex",
+        flexDirection: "column",
+        gap: 15,
+      }}
+    >
       <label htmlFor="beer">Search a beer:</label>
-      <input type="text" name='beer' value={searchInput} onChange={handleSearchInput} />
+      <input
+        type="text"
+        name="beer"
+        value={searchInput}
+        onChange={handleSearchInput}
+      />
       {allBeers.map((eachBeer) => {
         return (
-          <div key={eachBeer.name}  >
-          <Link to={eachBeer._id} style={{display: 'flex', gap: 30, textDecoration: "none"}}>
-            <img src={eachBeer.image_url} alt="foto-beer" width={40} />
-            <div>
-              <h3>{eachBeer.name}</h3>
-              <h5>{eachBeer.tagline}</h5>
-              <p>Created by: {eachBeer.name}</p>
-            </div>
-          </Link>     
-          <hr style={{width: "100%"}} />    
+          <div key={eachBeer.name}>
+            <Link
+              to={eachBeer._id}
+              style={{ display: "flex", gap: 30, textDecoration: "none" }}
+            >
+              <img src={eachBeer.image_url} alt="foto-beer" width={40} />
+              <div>
+                <h3>{eachBeer.name}</h3>
+                <h5>{eachBeer.tagline}</h5>
+                <p>Created by: {eachBeer.name}</p>
+              </div>
+            </Link>
+            <hr style={{ width: "100%" }} />
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-export default Beers
+export default Beers;
